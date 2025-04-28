bbf68c624f85594f91be693d97a974e7544985b8
Aibolat Konysbayev <aibolat.konysbayev@gmail.com>
2025-04-28 20:57:17 +0800

    Implement Job Posting Listing (List page, backend query, links)

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user, login_
 app = Flask(__name__)
 
 # --- НАСТРОЙКА SECRET_KEY (Шаг 19) ---
-app.config['SECRET_KEY'] = 'th5_nht2h_88ijh,_e_123dhd,_75mm_vvv_34m9men_.[ds8' # >>> ОБЯЗАТЕЛЬНО ЗАМЕНИ НА СВОЮ <<<
+app.config['SECRET_KEY'] = 'tyi,_htym_uk6_hm,_mkl._v54j_b8_ s5_nm,,' # >>> ОБЯЗАТЕЛЬНО ЗАМЕНИ НА СВОЮ <<<
 # --- КОНЕЦ НАСТРОЙКИ SECRET_KEY ---
 
 
@@ -47,8 +47,9 @@ class User(db.Model, UserMixin):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     # --- Связь с вакансиями (Шаг 21) ---
-    vacancies = db.relationship('Vacancy', backref='author', lazy=True)
-    # --- Конец Связи с вакансиями (Шаг 21) ---
+    # Добавим order_by для сортировки вакансий по дате создания по умолчанию
+    vacancies = db.relationship('Vacancy', backref='author', lazy=True, order_by="Vacancy.created_at.desc()")
+    # --- Конец Связи с вакансиями (Шаг 21, ИЗМЕНЕНО) ---
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -82,7 +83,55 @@ class Vacancy(db.Model):
 # Маршрут для отображения главной страницы
 @app.route('/')
 def index():
+     # Если пользователь авторизован, можно показать приветствие и ссылку на его вакансии
+     if current_user.is_authenticated:
+         flash(f'Привет, {current_user.email}!', 'success') # Пример приветствия
+         # TODO: Добавить ссылку на список вакансий в index.html
      return render_template('index.html', user=current_user)
+
+
+# --- НАЧАЛО НОВОГО КОДА: Маршрут Списка Вакансий (Шаг 23) ---
+@app.route('/jobs')
+@login_required # Только авторизованные пользователи могут видеть список
+def list_jobs():
+    # Получаем ВСЕ вакансии, где user_id совпадает с id текущего вошедшего пользователя
+    # Благодаря relationship в модели User, можно также сделать так: current_user.vacancies
+    # Query().all() возвращает список всех найденных объектов
+    user_vacancies = Vacancy.query.filter_by(user_id=current_user.id).all()
+    # Можно также использовать отношение, это часто более "питонично":
+    # user_vacancies = current_user.vacancies # Вакансии уже отсортированы по умолчанию в модели
+
+    # Передаем список вакансий в шаблон
+    return render_template('list_jobs.html', jobs=user_vacancies, user=current_user)
+# --- КОНЕЦ НОВОГО КОДА: Маршрут Списка Вакансий (Шаг 23) ---
+
+
+# Маршрут для Создания Вакансии (Шаг 22, ИЗМЕНЕНО перенаправление)
+@app.route('/jobs/create', methods=['GET', 'POST'])
+@login_required
+def create_job():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+
+        if not title or not description:
+            flash('Пожалуйста, заполните название и описание вакансии.', 'error')
+            return render_template('create_job.html', user=current_user)
+
+        new_job = Vacancy(
+            title=title,
+            description=description,
+            user_id=current_user.id
+        )
+
+        db.session.add(new_job)
+        db.session.commit()
+
+        flash('Вакансия успешно создана!', 'success')
+        # !!! ИЗМЕНЕНИЕ: Перенаправляем на страницу списка вакансий после создания !!!
+        return redirect(url_for('list_jobs'))
+
+    return render_template('create_job.html', user=current_user)
 
 
 # Маршрут для РЕГИСТРАЦИИ (Шаг 19)
@@ -139,7 +188,7 @@ def login():
             login_user(user)
             flash('Вы успешно вошли в систему!', 'success')
             # TODO: Возможно, добавить перенаправление на jobs
-            return redirect(url_for('index'))
+            return redirect(url_for('list_jobs')) # <<< ИЗМЕНЕНИЕ: Перенаправляем на список вакансий после входа
 
         else:
             flash('Неверный email или пароль.', 'error')
@@ -150,52 +199,11 @@ def login():
 
 # Маршрут для ВЫХОДА (LOGOUT) (Шаг 20)
 @app.route('/logout')
-@login_required # Этот маршрут доступен только авторизованным пользователям
+@login_required
 def logout():
     logout_user()
     flash('Вы вышли из системы.', 'success')
     return redirect(url_for('index'))
-
-
-# --- НАЧАЛО НОВОГО КОДА: Маршрут Создания Вакансии (Шаг 22) ---
-# @app.route('/jobs') # TODO: СЮДА ПОЗЖЕ МАРШРУТ ДЛЯ СПИСКА ВАКАНСИЙ
-# @login_required
-# def list_jobs():
-#    pass # TODO: Реализация списка вакансий
-
-@app.route('/jobs/create', methods=['GET', 'POST'])
-@login_required # Только авторизованные пользователи могут создавать вакансии
-def create_job():
-    if request.method == 'POST':
-        # Обработка данных формы при POST-запросе
-        title = request.form.get('title')
-        description = request.form.get('description')
-
-        # Базовая валидация
-        if not title or not description:
-            flash('Пожалуйста, заполните название и описание вакансии.', 'error')
-            # Передаем обратно на форму создания вакансии
-            return render_template('create_job.html', user=current_user) # Передаем user для шаблона
-
-        # Создаем новый объект Vacancy
-        new_job = Vacancy(
-            title=title,
-            description=description,
-            # !!! САМОЕ ВАЖНОЕ: Связываем вакансию с текущим вошедшим пользователем !!!
-            user_id=current_user.id # current_user - это объект пользователя, вошедшего в систему
-        )
-
-        # Добавляем вакансию в сессию базы данных и сохраняем
-        db.session.add(new_job)
-        db.session.commit()
-
-        flash('Вакансия успешно создана!', 'success')
-        # TODO: Перенаправить на страницу списка вакансий после создания
-        return redirect(url_for('index')) # Временно перенаправляем на главную
-
-    # Отображение формы создания вакансии при GET-запросе
-    return render_template('create_job.html', user=current_user)
-# --- КОНЕЦ НОВОГО КОДА: Маршрут Создания Вакансии (Шаг 22) ---
 
 
 # Маршрут для обработки данных от фронтенда (резюме и вакансия) - пока остается как было
